572a40d637a86bd2764399d14e8a16df098bfbc6
Sideron <66834276+Sideron@users.noreply.github.com>
2026-08-26 16:01:49 -0500

    Update task.controller.go

--- src/controller/task.controller.go
+++ src/controller/task.controller.go
@@ -3,10 +3,10 @@ package controller
 import "todo-go/src/models"
 
 type TaskController struct {
-	tasks []models.Task
+	tasks []*models.Task
 }
 
-func (c *TaskController) completeTask(name string) {
+func (c *TaskController) CompleteTask(name string) {
 	for i := range len(c.tasks) {
 		if c.tasks[i].Name() == name {
 			c.tasks[i].Complete()
@@ -16,15 +16,18 @@ func (c *TaskController) completeTask(name string) {
 }
 
 func (c *TaskController) Tasks() []models.Task {
-	return c.tasks
+	nTasks := make([]models.Task, 0, len(c.tasks))
+	for i := range len(c.tasks) {
+		nTasks = append(nTasks, *c.tasks[i])
+	}
+	return nTasks
 }
 
-func (c *TaskController) createNewTask(name string, description string) {
+func (c *TaskController) CreateNewTask(name string, description string) {
 	for i := range len(c.tasks) {
 		if c.tasks[i].Name() == name {
 			return
 		}
 	}
-	nTask := models.NewTask(name, description)
-	c.tasks = append(c.tasks, *nTask)
+	c.tasks = append(c.tasks, models.NewTask(name, description))
 }
